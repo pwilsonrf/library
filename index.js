61eb3ab424c65791085e4e5006f44c412696ae33
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-05-01 20:40:46 -0700

    Completed library sort through and book-shelf dislay, with some initial styling

--- index.js
+++ index.js
@@ -10,19 +10,46 @@ function addBookToLibrary (title, author, noOfPages, status, myLibrary){
 }
 
 function myLibraryLoop (myLibrary){
+    const bookShelf = document.querySelector('.book-shelf');
     for(i = 0; i < myLibrary.length; i++){
 
         //Create card
-        const currentBook = document.createElement("div");
+        let currentBook = document.createElement("div");
         currentBook.id = 'book' + i;
-        
-        
-        
-        const bookShelf = document.querySelector('#book-shelf');
+        currentBook.className = "book-item";
+
+        //Organize Book Data
+        let currentBookArray = myLibrary[i].split(","); //Separate data
+        console.log(currentBookArray)
+         
+        let currentBookTitle = document.createElement("h3");
+        currentBookTitle.innerText = currentBookArray[0];
+
+        let currentBookPages = document.createElement("p");
+        currentBookPages.innerText = currentBookArray[1];
+
+        let currentBookStatus = document.createElement("p");
+        currentBookStatus.id = "book-status"
+        currentBookStatus.innerText = currentBookArray[2];
+
+        //Push book elements into Div
+
+        currentBook.appendChild(currentBookTitle);
+        currentBook.appendChild(currentBookPages);
+        currentBook.appendChild(currentBookStatus);
         bookShelf.appendChild(currentBook);
+        
 
         currentBook = ""; //Delete previous entry
 
     }
 
 }
+
+
+addBookToLibrary ("Greatness", "Lewis Howes", 259, "Not Read", myLibrary)
+addBookToLibrary ("Brand With Purpose", "Ivan Estrada", 148, "In Progress", myLibrary)
+addBookToLibrary ("I will Teach you to Be Rich", "Ramit Sethi", 278, "Read", myLibrary)
+
+myLibraryLoop(myLibrary)
+console.log(myLibrary)
