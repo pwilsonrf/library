d180d708beb49f13f3a1a5cacfdbcca3065d46d7
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-05-01 23:56:39 -0700

    added pop up form and revised behavior to allow added books to show up on bookshelf

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let myLibrary = new Array();
+addBookToLibrary ("Greatness", "Lewis Howes", 259, "Not Read", myLibrary)
+addBookToLibrary ("Brand With Purpose", "Ivan Estrada", 148, "In Progress", myLibrary)
+addBookToLibrary ("I will Teach you to Be Rich", "Ramit Sethi", 278, "Read", myLibrary)
+myLibraryLoop(myLibrary);
 
 function Book(){
     
@@ -6,6 +10,34 @@ function Book(){
 
 function addBookToLibrary (title, author, noOfPages, status, myLibrary){
     myLibrary.push(`${title} by ${author}, ${noOfPages} pages, ${status}`);
+
+    const bookShelf = document.querySelector('.book-shelf');
+
+    //Create card
+    let currentBook = document.createElement("div");
+    currentBook.id = 'book' + myLibrary.length;
+    currentBook.className = "book-item";
+
+    //Organize Book Data
+    let currentBookArray = myLibrary[myLibrary.length - 1].split(","); //Separate data
+    console.log(currentBookArray)
+     
+    let currentBookTitle = document.createElement("h3");
+    currentBookTitle.innerText = currentBookArray[0];
+
+    let currentBookPages = document.createElement("p");
+    currentBookPages.innerText = currentBookArray[1];
+
+    let currentBookStatus = document.createElement("p");
+    currentBookStatus.id = "book-status"
+    currentBookStatus.innerText = currentBookArray[2];
+
+    //Push book elements into Div
+
+    currentBook.appendChild(currentBookTitle);
+    currentBook.appendChild(currentBookPages);
+    currentBook.appendChild(currentBookStatus);
+    bookShelf.appendChild(currentBook);
     return `${title} by ${author}, ${noOfPages} pafges, ${status}`;
 }
 
@@ -43,13 +75,32 @@ function myLibraryLoop (myLibrary){
         currentBook = ""; //Delete previous entry
 
     }
-
 }
 
 
-addBookToLibrary ("Greatness", "Lewis Howes", 259, "Not Read", myLibrary)
-addBookToLibrary ("Brand With Purpose", "Ivan Estrada", 148, "In Progress", myLibrary)
-addBookToLibrary ("I will Teach you to Be Rich", "Ramit Sethi", 278, "Read", myLibrary)
+//Pop up form when AddBook button is clicked
+let popUpForm = document.querySelector("#popUpForm"); //Pop-up Form
+var button = document.getElementById("addBook"); //Add Book Button
 
-myLibraryLoop(myLibrary)
+//Button behavior
+button.addEventListener("click", function () {
+    document.getElementById("popUpForm").style.display = "block";
+});
+
+
+
+//Grab information from form
+var titleValue = document.getElementById("form-id").addEventListener("submit", function (e){
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    addBookToLibrary(formProps.title, formProps.author, formProps.noOfPages, formProps.status, myLibrary)
+    console.log(formProps);
+})
+console.log(titleValue);
+
+
+
+
 console.log(myLibrary)
