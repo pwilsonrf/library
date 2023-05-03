8cfd1ff621d2ced15b179aeec9806f4652b4d09d
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-05-03 00:30:02 -0700

    Enhanced styling for Add Book form, styled Add Book button and started interactive behavior for deleting a book from library

--- index.js
+++ index.js
@@ -8,18 +8,34 @@ function Book(){
     
 }
 
+//Adding new book to existing Library array
 function addBookToLibrary (title, author, noOfPages, status, myLibrary){
     myLibrary.push(`${title} by ${author}, ${noOfPages} pages, ${status}`);
+
+}
+
+
+//Displaying existing Library array
+function myLibraryLoop (myLibrary){
+    const bookShelf = document.querySelector('.book-shelf');
+    for(i = 0; i < myLibrary.length; i++){
+        addBookToShelf(myLibrary[i], i)
+    }
+}
+
+
+//Displaying a book on shelf
+function addBookToShelf(book, i){
 
     const bookShelf = document.querySelector('.book-shelf');
 
     //Create card
     let currentBook = document.createElement("div");
-    currentBook.id = 'book' + myLibrary.length;
+    currentBook.id = 'book' + i;
     currentBook.className = "book-item";
 
     //Organize Book Data
-    let currentBookArray = myLibrary[myLibrary.length - 1].split(","); //Separate data
+    let currentBookArray = book.split(","); //Separate data
     console.log(currentBookArray)
      
     let currentBookTitle = document.createElement("h3");
@@ -32,55 +48,43 @@ function addBookToLibrary (title, author, noOfPages, status, myLibrary){
     currentBookStatus.id = "book-status"
     currentBookStatus.innerText = currentBookArray[2];
 
-    //Push book elements into Div
 
+    //Create delete button
+    let currentBookDeleteButton = document.createElement("div");
+    currentBookDeleteButton.id = `delete-book-button${i}`;
+    currentBookDeleteButton.className = "delete-book-button";
+
+    let deleteImg = document.createElement("img");
+    deleteImg.src = "/img/trash-can-outline.svg";
+    deleteImg.alt = `delete book icon ${i}`;
+    currentBookDeleteButton.appendChild(deleteImg);
+    
+    
+    
+
+    //Push book elements into Div
+    currentBook.appendChild(currentBookDeleteButton);
     currentBook.appendChild(currentBookTitle);
     currentBook.appendChild(currentBookPages);
     currentBook.appendChild(currentBookStatus);
+    
     bookShelf.appendChild(currentBook);
-    return `${title} by ${author}, ${noOfPages} pafges, ${status}`;
-}
+    
 
-function myLibraryLoop (myLibrary){
-    const bookShelf = document.querySelector('.book-shelf');
-    for(i = 0; i < myLibrary.length; i++){
+    currentBook = ""; //Delete previous entry
 
-        //Create card
-        let currentBook = document.createElement("div");
-        currentBook.id = 'book' + i;
-        currentBook.className = "book-item";
-
-        //Organize Book Data
-        let currentBookArray = myLibrary[i].split(","); //Separate data
-        console.log(currentBookArray)
-         
-        let currentBookTitle = document.createElement("h3");
-        currentBookTitle.innerText = currentBookArray[0];
-
-        let currentBookPages = document.createElement("p");
-        currentBookPages.innerText = currentBookArray[1];
-
-        let currentBookStatus = document.createElement("p");
-        currentBookStatus.id = "book-status"
-        currentBookStatus.innerText = currentBookArray[2];
-
-        //Push book elements into Div
-
-        currentBook.appendChild(currentBookTitle);
-        currentBook.appendChild(currentBookPages);
-        currentBook.appendChild(currentBookStatus);
-        bookShelf.appendChild(currentBook);
-        
-
-        currentBook = ""; //Delete previous entry
-
-    }
+    //Delete Book Behavior
+    document.getElementById(currentBookDeleteButton.id).addEventListener("click", function (e){
+        console.log(e.target);
+    
+});
+    
 }
 
 
 //Pop up form when AddBook button is clicked
 let popUpForm = document.querySelector("#popUpForm"); //Pop-up Form
-var button = document.getElementById("addBook"); //Add Book Button
+let button = document.getElementById("addBook"); //Add Book Button
 
 //Button behavior
 button.addEventListener("click", function () {
@@ -90,14 +94,30 @@ button.addEventListener("click", function () {
 
 
 //Grab information from form
-var titleValue = document.getElementById("form-id").addEventListener("submit", function (e){
+let titleValue = document.getElementById("form-id").addEventListener("submit", function (e){
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
 
-    addBookToLibrary(formProps.title, formProps.author, formProps.noOfPages, formProps.status, myLibrary)
+    //Add book to library
+    addBookToLibrary(formProps.title, formProps.author, formProps.noOfPages, formProps.status, myLibrary);
+    addBookToShelf(myLibrary[myLibrary.length-1]);
     console.log(formProps);
+
+    //Reset form
+    document.getElementById("form-id").reset();
+
+    //Hide form after submission
+    document.getElementById("popUpForm").style.display = "none";
+
+
+
 })
+
+
+
+
+
 console.log(titleValue);
 
 
